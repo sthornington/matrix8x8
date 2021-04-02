89201686b768f5a8a730e676287268bbfab12199
Simon Thornington <simon.thornington@gmail.com>
2021-04-02 10:36:30 -0400

    added minimal repro

--- src/debounce.sv
+++ src/debounce.sv
@@ -3,22 +3,24 @@
 module debounce(input logic  clk,
                 input logic  i_btn,
                 output logic o_btn);
-    // 0x03ffff is about 1/4 of a second
+    // 0x03ffff is ~1/4 of a second
     localparam WIDTH = 18;
-    //localparam MAX = {WIDTH{1'b1}};
     localparam MAX = 249999;
-    reg [WIDTH-1:0] counter = 0;
-    reg clk_en;
+    reg [WIDTH-1:0] counter=0;
+    wire clk_en;
 
     always_ff @(posedge clk) begin
-        counter <= (counter == MAX) ? 0 : counter + 1;
+// FAILS using >= "ERROR: Visited AIG node more than once; this could be a combinatorial loop that has not been broken - see Yosys bug 2530"
+//      counter <= (counter >= MAX) ? 0 : counter+1;
+// WORKS using ==
+      counter <= (counter == MAX) ? 0 : counter+1;
     end
 
     assign clk_en = (counter == MAX) ? 1'b1 : 1'b0;
 
-    reg q0;
-    reg q1;
-    reg q2;
+    reg        q0;
+    reg        q1;
+    reg        q2;
 
     always_ff @(posedge clk) begin
         if (clk_en == 1'b1) begin
